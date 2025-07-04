0416ed7bed6c5e1fdc533be5fd938b9f740700a1
Mirnes Becirbegovic <mirnesbb@gmail.com>
2025-07-04 18:12:22 +0200

    Add geocode utility for location lookup

---
+++ weather-app/utils/geocode.js
@@ -0,0 +1,35 @@
+require("dotenv").config();
+
+const request = require("postman-request");
+
+const geoCode = (address, callback) => {
+  const baseUrl = process.env.MAPBOX_BASE_URL;
+  const token = process.env.MAPBOX_TOKEN;
+
+  const url = `${baseUrl}${encodeURIComponent(
+    address
+  )}.json?access_token=${token}&limit=1`;
+
+  request({ url: url, json: true }, (error, res) => {
+    if (error) {
+      callback("Unable to connect to location service!");
+    } else if (res.body.features.length === 0) {
+      callback("Unable to find location! Try another search");
+    } else {
+      callback(undefined, {
+        location: res.body.features[0].place_name,
+        longitude: res.body.features[0].center[0],
+        latitude: res.body.features[0].center[1],
+      });
+    }
+  });
+};
+
+// geoCode("Sarajevo", (error, data) => {
+//   console.log("Data:", data);
+//   console.log("Error:", error);
+// });
+
+module.exports = {
+    geoCode: geoCode
+}
