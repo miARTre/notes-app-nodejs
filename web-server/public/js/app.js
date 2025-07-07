8be166cdb9375d2a87d003cbeb04d4b46ee8a509
Mirnes Becirbegovic <mirnesbb@gmail.com>
2025-07-07 13:09:29 +0200

    exploring fetch

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,33 @@
 console.log("Client side JavaScript file is loaded!");
+
+fetch("http://puzzle.mead.io/puzzle").then((response) => {
+  response.json().then((data) => {
+    console.log(data);
+  });
+});
+
+// fetch("http://localhost:3000/weather?address")
+//   .then((response) => response.json())
+//   .then(({ error, forecast, location }) => {
+//     if (error) {
+//       console.log("Error:", error);
+//     } else {fcc
+//       console.log("Forecast:", forecast);
+//       console.log("Location:", location);
+//     }
+//   })
+//   .catch((err) => {
+//     console.log("Fetch error:", err);
+//   });
+
+
+fetch('http://localhost:3000/weather?address=sarajevo').then((response) => {
+    response.json().then((data) => {
+        if (data.error) {
+            console.log(data.error)
+        } else {
+            console.log(data.location)
+            console.log(data.forecast)
+        }
+    })
+})
